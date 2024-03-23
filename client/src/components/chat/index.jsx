62ad23a4c871a391541bbab326cb895b6f45e4d2
Karmvir <jadejakarmvir12@gmail.com>
2024-03-23 16:09:11 +0530

    Here i have nearly completed the frontend work customized the input for and submit function of the web app now i can send and txt and image and can save

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -4,25 +4,32 @@ import { useMultiChatLogic,
   MultiChatWindow,
 } from 'react-chat-engine-advanced';
 import Header from "@/components/customHeader";
-
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
 
 const Chat = () => {
   const chatProps =useMultiChatLogic(
-    import.meta.env.VITE_PROJECT_ID,
+
+    "9b6feca7-429a-425f-8bee-dfd3e46a8231",
     "testuser",
     "1234"
   );
+    
 
-
-  return <div style={{flexBasis:"100%"}}>
+  return( 
+  <div style={{flexBasis:"100%"}}>
     <MultiChatSocket {...chatProps}/>
     <MultiChatWindow 
       {...chatProps}
       style={{height:"100vh"}}
       renderChatHeader={(chat)=> <Header chat={chat}/>}
+      renderMessageForm={(props)=>{
+        return(
+          <StandardMessageForm props={props} activeChat={chatProps.chat}/>
+        );
+      }}
     />
   </div>
-
+  )
 };
 
 export default Chat;
